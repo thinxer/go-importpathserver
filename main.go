12779873d2cdf5abbd57223e0d534971750bc92a
Jianfei Wang <me@thinxer.com>
2014-12-20 17:50:30 +0800

    rewrite

--- main.go
+++ main.go
@@ -2,40 +2,38 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"html/template"
 	"net/http"
+	"regexp"
 )
 
-var tmpl = template.Must(template.New("").Parse(`
-<html>
-<meta name="go-import" content="{{.Prefix}} {{.Vcs}} {{.Repo}}">
-<body>
-</body>
-</html>
-`))
+var tmpl = template.Must(template.New("").Parse(`<meta name="go-import" content="{{.MetaImport}}">`))
 
 var (
-	flagListen       = flag.String("listen", "localhost:8000", "address to listen on")
-	flagImportPrefix = flag.String("prefix", "github.com", "import prefix")
-	flagVCS          = flag.String("vcs", "git", "vcs")
-	flagVCSPattern   = flag.String("pattern", "git@github.com:%s.git", "vcs pattern")
+	flagListen      = flag.String("listen", "localhost:8000", "address to listen on")
+	flagPathPattern = flag.String("path", "/x/(?P<proj>[^/]+).*", "path pattern")
+	flagMetaImport  = flag.String("meta", "txr.me/x/$proj git ssh://git@github.com/thinxer/$proj.git", "go import meta")
 )
 
 type Handler struct {
+	PathPattern *regexp.Regexp
+	MetaImport  string
 }
 
 func (h *Handler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	path := req.URL.Path
 	tmpl.Execute(rw, struct {
-		Prefix string
-		Vcs    string
-		Repo   string
-	}{*flagImportPrefix + path, *flagVCS, fmt.Sprintf(*flagVCSPattern, path[1:])})
+		MetaImport string
+	}{
+		h.PathPattern.ReplaceAllString(path, h.MetaImport),
+	})
 }
 
 func main() {
 	flag.Parse()
-	h := &Handler{}
+	h := &Handler{
+		regexp.MustCompile(*flagPathPattern),
+		*flagMetaImport,
+	}
 	panic(http.ListenAndServe(*flagListen, h))
 }
